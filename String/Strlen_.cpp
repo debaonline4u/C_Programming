8ea37e744ffac21e15f8073b161b4a695a61a1e5
debaonline4u <debashis.sahoo88@gmail.com>
2018-03-16 01:09:14 +0530

    Strlen doesn't count NULL value

--- String/Strlen_.cpp
+++ String/Strlen_.cpp
@@ -2,7 +2,7 @@
 #include<string.h>  // This program prints Length of String by Strlen(), it doesn't count \0 value
 int main()
 {
-	char s[]="Debashis";
+	char s[]="Debashis \0sahoo";
 	char *t="Papani";
 	printf("First String Length=%d\n",strlen(s));	// Doesn't count \0 value. 
 	printf("Second Stirng Lengh=%d\n",strlen(t));
